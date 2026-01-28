6e99e204251e4cd7c9c518768b83f397f0a381b8
Rahul V S <rahulshaju135@gmail.com>
2026-01-28 12:45:42 +0530

    adad

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "./", // 👈 ADD THIS
   plugins: [react(), tailwindcss()],
 });
